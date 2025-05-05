ecc44db6e65402fffc463ca834c45188ac9b2e49
leiandrei <leiandreiedomaoal@iskolarngbayan.pup.edu.ph>
2025-05-05 19:39:36 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void sort(float *data, int num);
+void print(float *data, int num);
+float mean(float *data, int num);
+float median(float *data, int num);
+float mode(float *data, int num);
+float std_dev(float *data, int num);
+
+int main()
+{
+    int num;
+
+    printf("Enter the number of the datasets: ");
+    scanf("%d", &num);
+
+    if (num <= 0) { // input validation
+        printf("Invalid Input.\n");
+        printf("Exiting Program...\n");
+        return 1;
+    }
+
+    float *datasets = calloc(num, sizeof(float)); // dynamic memory allocation (malloc = random values) 
+
+    if (!datasets) {
+        fprintf(stderr, "Memory Allocation Failed.\n");
+        return 1;
+    }
+
+    printf("\nEnter the following datasets:\n");
+    for (int i = 0; i < num; i++)
+    {
+        printf("Data Stored[%d]: ", i + 1); // index starts at 1
+        scanf("%f", &datasets[i]);
+    }
+
+    sort(datasets, num);
+    print(datasets, num);
+
+    printf("\n=== Statistical Results ===\n");
+    printf("Mean: %.2f\n", mean(datasets, num));
+    printf("Median: %.2f\n", median(datasets, num));
+    printf("Mode: %.2f\n", mode(datasets, num));
+    printf("Standard Deviation: %.2f\n", std_dev(datasets, num));
+
+
+    free(datasets); // frees the used memory
+    return 0;
+}
+
+void sort(float *data, int num)
+{
+    for (int i = 1; i < num; i++) {
+        float curr = *(data + i);
+        int j = i - 1;
+
+        while (j >= 0 && *(data + j) > curr) {
+            *(data + j + 1) = *(data + j);
+            j--;
+        }
+        *(data + j + 1) = curr;
+    }
+}
+
+void print(float *data, int num)
+{
+    printf("\nThe Following Datasets (Sorted):\n");
+    for (int i = 0; i < num; i++) {
+        printf("%.2f ", *(data +i));
+    }
+    printf("\n");
+}
+
+float mean(float *data, int num)
+{
+    float average = 0;
+    for (int i = 0; i < num; i++)
+    {
+        average += *(data + i) / num;
+    }
+    return (float)average;
+}
+
+float median(float *data, int num)
+{
+
+    if (num % 2 == 0) {
+        float mid1 = data[(num / 2) - 1];
+        float mid2 = data[num / 2];
+        return (mid1 + mid2) / 2.0;
+    } else {
+        return data[num / 2];
+    }
+}
+
+float mode(float *data, int num)
+{
+    float max_val = 0, max_count = 0;
+    for (int i = 0; i < num; i++)
+    {
+        float count = 0;
+        for (int j = 0; j < num; j++) {
+            if (*(data + i) == *(data + j)) {
+                ++count;
+            }
+        }
+        if (count > max_count) {
+            max_count = count;
+            max_val = *(data + i);
+        }
+
+    }
+
+    if (max_count == 1) {
+        printf("No mode.\n");
+    }
+
+    return max_val;
+}
+
+float std_dev(float *data, int num)
+{
+    float variance = 0, standard_deviation;
+    float accumulated_mean = mean(data, num);
+
+    for (int i = 0; i < num; i++) {
+        variance += pow(*(data + i) - accumulated_mean, 2);
+    }
+
+    variance /= (num - 1);
+    standard_deviation = sqrt(variance);
+
+    return standard_deviation;
+}
